39bbfc150e5bf176a5fd6b95f89a5c41ad5cdb4e
Lee Briggs <lee@leebriggs.co.uk>
2022-10-21 13:20:35 -0700

    add private/public

--- vpc/index.ts
+++ vpc/index.ts
@@ -5,4 +5,9 @@ import * as awsx from "@pulumi/awsx";
 // Create an AWS resource (S3 Bucket)
 const vpc = new awsx.ec2.Vpc("scratch", {
     cidrBlock: "172.16.0.0/22",
+    subnetSpecs: [{
+        type: "Public",
+    }, {
+        type: "Private",
+    }]
 })
