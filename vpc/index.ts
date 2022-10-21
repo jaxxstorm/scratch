abfc4dcf60bff83a93c7d48d79bc00922fa27673
Lee Briggs <lee@leebriggs.co.uk>
2022-10-21 13:09:52 -0700

    add a vpc

---
+++ vpc/index.ts
@@ -0,0 +1,8 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+// Create an AWS resource (S3 Bucket)
+const vpc = new awsx.ec2.Vpc("scratch", {
+    cidrBlock: "172.16.0.0/22",
+})
